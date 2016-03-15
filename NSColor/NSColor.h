3f8912777458c4f84335cd9b8b659b9b43a0302b
DRAFT <draft-ad@draft-addemacbook-pro.local>
2016-03-15 10:52:25 -0700

    Initial Commit

---
+++ NSColor/NSColor.h
@@ -0,0 +1,19 @@
+//
+//  NSColor.h
+//  NSColor
+//
+//  Created by DRAFT-AD on 16/3/15.
+//  Copyright © 2016年 DRAFT-AD. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for NSColor.
+FOUNDATION_EXPORT double NSColorVersionNumber;
+
+//! Project version string for NSColor.
+FOUNDATION_EXPORT const unsigned char NSColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSColor/PublicHeader.h>
+
+
